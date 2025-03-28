7e90629a5f929c2a9eac567031f1fb4ee438ed8b
AmrrTarekk <amrrtarek73@gmail.com>
2025-03-28 13:51:47 +0200

    Updated with a submodule

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,14 @@ import ErrorPage from "./pages/NotFound";
 import LoadingComp from "./components/LoadingComponent";
 import DetailsLayout from "./layout/DetailsLayout";
 
+// Lazy load first-app pages
 const Home = lazy(() => import("./pages/Home"));
 const ViewJobDetails = lazy(() => import("./pages/ViewJobDetails"));
 const ViewSkillDetails = lazy(() => import("./pages/ViewSkillDetails"));
 const SearchJobs = lazy(() => import("./pages/SearchJobs"));
+
+// Lazy load second-app from submodule
+const SecondApp = lazy(() => import("../book-world/src/App"));
 
 function App() {
   const router = createBrowserRouter([
@@ -66,13 +70,17 @@ function App() {
         },
       ],
     },
+    {
+      path: `/second-app/*`, // Load second-app inside /second-app route
+      element: (
+        <Suspense fallback={<LoadingComp />}>
+          <SecondApp />
+        </Suspense>
+      ),
+    },
   ]);
 
-  return (
-    <>
-      <RouterProvider router={router} />
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
